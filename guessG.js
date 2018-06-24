ad1e8feba5e29e210809724d2d1634b2df1f9e74
djmorosini <djmorosini@outlook.com>
2018-06-24 15:00:38 -0400

    changed output in Hguess game

--- guessG.js
+++ guessG.js
@@ -54,59 +54,56 @@ function runHgame() {
     if (isNaN(humanGuess)) {
         say("I am Groot!")
         say("'Please enter a number!'");
-    }
-    else if (humanGuess == 0) {
+    } else if (humanGuess == 0) {
         say("I am Groot!")
         say("'You have to enter a number!'");
-    }
-    else if (humanGuess < minG) {
+    } else if (humanGuess < minG) {
         say("I am Groot.")
         say("'I already told you it can't be lower than " + minG + ".'");
-    }
-    else if (humanGuess > maxG) {
+    } else if (humanGuess > maxG) {
         say("I am Groot.")
         say("'I already told you it can't be higher than " + maxG + ".'");
-    }
-    else if (humanGuess < computerNumber) {
+    } else if (humanGuess < computerNumber) {
         if (maxG != 100) {
-            say("Nope, it's higher than " + humanGuess + " but lower than " + maxG + "! Guess again.");
+            say("I am Groot!")
+            say("'Nope, it's higher than " + humanGuess + " but lower than " + maxG + "!' Guess again.");
+            minG = humanGuess;
+            trys++;
+        } else {
+            say("I am Groot!")
+            say("'Nope, it's higher than " + humanGuess + " but lower than, or including " + maxG + "!' Guess again.");
             minG = humanGuess;
             trys++;
         }
-        else {
-            say("Nope, it's higher than " + humanGuess + " but lower than, or including " + maxG + "! Guess again.");
-            minG = humanGuess;
-            trys++;
-        }
-    }
-    else if (humanGuess > computerNumber) {
+    } else if (humanGuess > computerNumber) {
         if (minG != 1) {
-            say("Nope, it's lower than " + humanGuess + " but higher than " + minG + "! Guess again.");
+            say("I am Groot!")
+            say("'Nope, it's lower than " + humanGuess + " but higher than " + minG + "!' Guess again.");
+            maxG = humanGuess;
+            trys++;
+        } else {
+            say("I am Groot!")
+            say("'Nope, it's lower than " + humanGuess + " but higher than, or including " + minG + "!' Guess again.");
             maxG = humanGuess;
             trys++;
         }
-        else {
-            say("Nope, it's lower than " + humanGuess + " but higher than, or including " + minG + "! Guess again.");
-            maxG = humanGuess;
-            trys++;
-        }
-    }
-    else if (humanGuess == computerNumber) {
+    } else if (humanGuess == computerNumber) {
         if (trys < numberOfGuesses) {
-            say("You guessed it in " + trys + " tries! Congratulations on guessing it in less than " + numberOfGuesses + " tries!");
+            say("I am Groot!")
+            say("'You guessed it in " + trys + " tries! Congratulations on guessing it in less than " + numberOfGuesses + " tries!'");
+            exit();
+        } else if (trys > numberOfGuesses) {
+            say("I am Groot.")
+            say("'It took " + trys + " tries. That's more than " + numberOfGuesses + " tries, better luck next time.'");
+            exit();
+        } else if (trys == numberOfGuesses) {
+            say("I am Groot.")
+            say("'It took exactly " + trys + " out of " + numberOfGuesses + " tries. Nice job!'");
             exit();
         }
-        else if (trys > numberOfGuesses) {
-            say("It took " + trys + " tries. That's more than " + numberOfGuesses + " tries, better luck next time.");
-            exit();
-        }
-        else if (trys == numberOfGuesses) {
-            say("It took exactly " + trys + " out of " + numberOfGuesses + " tries. Nice job!");
-            exit();
-        }
-    }
-    else {
-        say("What?");
+    } else {
+        say("I am Groot?")
+        say("'What?'");
         exit();
     }
 }
